19e7632f50c092347519f1065903924e1cb77e14
AayushPokharel <official@aayushpokharel.com>
2022-09-03 22:58:54 +0545

    first commit

---
+++ src/sphinxdoc/urls.py
@@ -0,0 +1,51 @@
+"""
+URL conf for django-sphinxdoc.
+
+"""
+from django.urls import re_path
+
+from . import views
+
+
+re_pathpatterns = [
+    re_path(
+        r'^$',
+        views.OverviewList.as_view(),
+        name='docs-list',
+    ),
+    re_path(
+        r'^(?P<slug>[\w-]+)/search/$',
+        views.ProjectSearchView(),
+        name='doc-search',
+    ),
+    # These re_paths have to be without the / at the end so that relative links in
+    # static HTML files work correctly and that browsers know how to name files
+    # for download
+    re_path(
+        (r'^(?P<slug>[\w-]+)/(?P<type_>_images|_static|_downloads|_source)/'
+         r'(?P<path>.+)$'),
+        views.sphinx_serve,
+    ),
+    re_path(
+        r'^(?P<slug>[\w-]+)/_objects/$',
+        views.objects_inventory,
+        name='objects-inv',
+    ),
+    re_path(
+        r'^(?P<slug>[\w-]+)/$',
+        views.documentation,
+        {'path': ''},
+        name='doc-index',
+    ),
+    re_path(
+        r'^(?P<slug>[\w-]+)/genindex/$',
+        views.documentation,
+        {'path': 'genindex'},
+        name='doc-genindex',
+    ),
+    re_path(
+        r'^(?P<slug>[\w-]+)/(?P<path>.+)/$',
+        views.documentation,
+        name='doc-detail',
+    ),
+]
